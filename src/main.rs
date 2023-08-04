d00793d02b93bd08d7d27f6d5de89973d0bd93c7
One <43485962+c-git@users.noreply.github.com>
2023-08-03 23:04:03 -0400

    Start code that uses linux ping tool

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,108 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::Command;
+
+use anyhow::{bail, Context};
+use regex::Regex;
+
+fn main() -> anyhow::Result<()> {
+    let targets = vec![
+        Target::new("127.0.0.1".to_string(), None),
+        Target::new("8.8.8.8".to_string(), None),
+        Target::new("192.168.1.205".to_string(), None),
+        Target::new("192.168.8.8".to_string(), None),
+    ];
+
+    for target in targets {
+        let result = ping(&target);
+        let _ = dbg!(result);
+        println!("-----");
+    }
+    Ok(())
+}
+
+/// Finds the round trip time to the target if less than timeout
+fn ping(target: &Target) -> anyhow::Result<PingResponse> {
+    // TODO Make regex only get compiled once
+    let re_pass =
+        Regex::new(r"icmp_seq=\d+ ttl=\d+ time=(\d+)\.(\d+) ms").expect("Failed to compile regex");
+    let re_fail =
+        Regex::new(r"bytes of data.\n(?:(.*)\n)?\n---.*\n1 packets transmitted, 0 received")
+            .expect("Failed to compile regex");
+    let output = Command::new("ping")
+        .arg("-c")
+        .arg("1")
+        .arg(&target.host)
+        .output()
+        .context("Failed to execute ping")?;
+    let stdout = std::str::from_utf8(&output.stdout).context("Failed to convert stdout to ut8")?;
+    // TODO check stderr and log if it is not empty
+    if let Some(captures) = re_pass.captures(stdout) {
+        // Regex matched and can only match if both capture groups are found as they are not optional
+        let ms = captures.get(1).unwrap();
+        let ms_frac = captures.get(2).unwrap();
+
+        Ok(PingResponse::Time(Milliseconds::try_from((
+            ms.as_str(),
+            ms_frac.as_str(),
+        ))?))
+    } else if let Some(captures) = re_fail.captures(stdout) {
+        match captures.get(1) {
+            Some(error_msg) => Ok(PingResponse::Error {
+                msg: error_msg.as_str().to_owned(),
+            }),
+            None => Ok(PingResponse::Timeout),
+        }
+    } else {
+        bail!("stdout did not match pass nor fail. stdout: {stdout:?}");
+    }
+}
+
+struct Target {
+    host: String,
+
+    /// If supplied overrides the global default timeout for waiting for a response
+    timeout: Option<Seconds>,
+}
+
+impl Target {
+    fn new(host: String, timeout: Option<Seconds>) -> Self {
+        Self { host, timeout }
+    }
+}
+
+#[derive(Debug)]
+struct Milliseconds(u16);
+
+#[derive(Debug)]
+struct Seconds(u8);
+
+#[derive(Debug)]
+enum PingResponse {
+    Time(Milliseconds),
+    Timeout,
+    Error { msg: String },
+}
+
+impl From<u16> for Milliseconds {
+    fn from(value: u16) -> Self {
+        Self(value)
+    }
+}
+
+impl From<u8> for Seconds {
+    fn from(value: u8) -> Self {
+        Self(value)
+    }
+}
+
+impl TryFrom<(&str, &str)> for Milliseconds {
+    type Error = anyhow::Error;
+
+    fn try_from((ms, ms_frac): (&str, &str)) -> Result<Self, Self::Error> {
+        let mut ms: u16 = ms.parse().context("Failed to parse ms in ping")?;
+        let ms_frac: u16 = ms_frac.parse().context("Failed to parse ms in ping")?;
+        if ms_frac >= 50 {
+            ms += 1;
+        }
+        Ok(Self(ms))
+    }
 }
