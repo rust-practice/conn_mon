d1f10c86d75c8b1f83cbc9e27c6fcc4633ffed43
One <43485962+c-git@users.noreply.github.com>
2023-08-10 02:42:14 -0400

    Display more info for debugging

--- src/lib.rs
+++ src/lib.rs
@@ -15,9 +15,10 @@ pub fn run(cli: Cli) -> anyhow::Result<()> {
     let config = Config::load_from(&cli.get_config_path()).context("Failed to load config")?;
 
     for target in config.targets.iter() {
+        dbg!(target);
         let result = ping(target, &config.default_timeout);
         let _ = dbg!(result);
-        println!("-----");
+        println!("----------------------------------------------");
     }
     println!("Completed");
     Ok(())
