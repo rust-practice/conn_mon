4c09034db1fbb775440f2509cb460c13c19a3e4d
One <43485962+c-git@users.noreply.github.com>
2023-08-20 22:55:10 -0400

    WIP Realized I was supposed to send all ping responses

--- src/state_management.rs
+++ src/state_management.rs
@@ -5,12 +5,19 @@ use crate::{
     ping::PingResponse,
 };
 
-pub struct EventPublisher {}
+pub struct EventPublisher {
+    target_id: TargetID,
+    tx: Sender<EventMessage>,
+    state: MonitorState,
+}
 
 impl EventPublisher {
     pub fn new(target_id: TargetID, tx: Sender<EventMessage>) -> Self {
-        todo!();
-        Self {}
+        Self {
+            target_id,
+            tx,
+            state: MonitorState::new(),
+        }
     }
 
     pub fn process_response(&mut self, response: PingResponse) {
@@ -18,4 +25,13 @@ impl EventPublisher {
     }
 }
 
-enum MonitorState {}
+enum MonitorState {
+    Start,
+    Up,
+}
+
+impl MonitorState {
+    fn new() -> Self {
+        Self::Start
+    }
+}
