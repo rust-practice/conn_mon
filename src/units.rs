e476725f904bdca0c494c1f924ff21d31c8c4934
One <43485962+c-git@users.noreply.github.com>
2023-08-21 10:43:36 -0400

    Upgrade unit types to u64
    Simpler than trying to downcast from u64 and figure out what to do on
    out of bounds. Values later than u8 are surely possible for downtime,
    wasn't as likely when it was just for configs.

--- src/units.rs
+++ src/units.rs
@@ -4,31 +4,31 @@ use anyhow::Context;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
-pub struct Milliseconds(u16);
+pub struct Milliseconds(u64);
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize, Clone, Copy)]
-pub struct Seconds(u8);
+pub struct Seconds(u64);
 impl Display for Seconds {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0)
     }
 }
 
-impl From<u16> for Milliseconds {
-    fn from(value: u16) -> Self {
-        Self(value)
-    }
-}
-
-impl From<u8> for Seconds {
-    fn from(value: u8) -> Self {
+impl From<u64> for Milliseconds {
+    fn from(value: u64) -> Self {
         Self(value)
     }
 }
 
 impl From<Seconds> for u64 {
     fn from(value: Seconds) -> Self {
-        value.0 as u64
+        value.0
+    }
+}
+
+impl From<u64> for Seconds {
+    fn from(value: u64) -> Self {
+        Self(value)
     }
 }
 
@@ -36,8 +36,8 @@ impl TryFrom<(&str, &str)> for Milliseconds {
     type Error = anyhow::Error;
 
     fn try_from((ms, ms_frac): (&str, &str)) -> Result<Self, Self::Error> {
-        let mut ms: u16 = ms.parse().context("Failed to parse ms in ping")?;
-        let ms_frac: u16 = ms_frac.parse().context("Failed to parse ms_frac in ping")?;
+        let mut ms: u64 = ms.parse().context("Failed to parse ms in ping")?;
+        let ms_frac: u64 = ms_frac.parse().context("Failed to parse ms_frac in ping")?;
         if ms_frac >= 50 {
             ms += 1;
         }
